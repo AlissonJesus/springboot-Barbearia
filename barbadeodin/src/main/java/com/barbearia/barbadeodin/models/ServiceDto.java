096513d3cdf843e8a19189fb25afaee67720a920
AlissonJesus <alissonalves132@gmail.com>
2024-07-18 16:34:04 -0300

    feat: include validations in ServiceDto

--- barbadeodin/src/main/java/com/barbearia/barbadeodin/models/ServiceDto.java
+++ barbadeodin/src/main/java/com/barbearia/barbadeodin/models/ServiceDto.java
@@ -1,9 +1,17 @@
 package com.barbearia.barbadeodin.models;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public record ServiceDto(
+		
+		@NotBlank
 		String name, 
 		String Description, 
+		
+		@NotNull
 		double price, 
-		int duration) {
+		@NotNull
+		Integer duration) {
 	
 }
