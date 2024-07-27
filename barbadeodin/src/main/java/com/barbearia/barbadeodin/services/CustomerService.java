9245b3d98167e31df3ffe8c5a07697ed304dbcae
AlissonJesus <alissonalves132@gmail.com>
2024-07-27 05:14:06 -0300

    feat: ADD the register method in the CustomerService class

--- barbadeodin/src/main/java/com/barbearia/barbadeodin/services/CustomerService.java
+++ barbadeodin/src/main/java/com/barbearia/barbadeodin/services/CustomerService.java
@@ -2,17 +2,23 @@ package com.barbearia.barbadeodin.services;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.barbearia.barbadeodin.dto.CustomerRequestDto;
 import com.barbearia.barbadeodin.dto.CustomerResponseDto;
+import com.barbearia.barbadeodin.models.Customer;
+import com.barbearia.barbadeodin.repositories.CustomerRepository;
 
 @Service
 public class CustomerService {
+	@Autowired
+	private CustomerRepository repository;
 
 	public CustomerResponseDto register(CustomerRequestDto requestBody) {
-		// TODO Auto-generated method stub
-		return null;
+		Customer customer = new Customer(requestBody);
+		customer = repository.save(customer);
+		return createCustomerResponseDto(customer);
 	}
 
 	public CustomerResponseDto getById(Long id) {
@@ -34,6 +40,10 @@ public class CustomerService {
 	public void deleteById(Long any) {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	private CustomerResponseDto createCustomerResponseDto(Customer customer) {
+		return new CustomerResponseDto(customer);
 	}
 
 }
